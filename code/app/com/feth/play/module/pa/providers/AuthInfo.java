400b6ef570a404bb7b91af93240278a00bdacbe5
Joscha Feth <joscha@feth.com>
2012-06-18 16:18:27 +0200

    Initial commit
    Former-commit-id: 883c51933c35e1cbf7fea469f77c9c6a2cbc2c2b

---
+++ code/app/com/feth/play/module/pa/providers/AuthInfo.java
@@ -0,0 +1,26 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_package
+package|package
+name|com
+operator|.
+name|feth
+operator|.
+name|play
+operator|.
+name|module
+operator|.
+name|pa
+operator|.
+name|providers
+package|;
+end_package
+
+begin_class
+specifier|public
+class|class
+name|AuthInfo
+block|{  }
+end_class
+
+end_unit
+
