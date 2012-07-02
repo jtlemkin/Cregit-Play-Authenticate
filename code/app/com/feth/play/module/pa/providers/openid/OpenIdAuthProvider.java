c42a3f9f8480be8fd4aaddf4f91e9ba8c4d6544d
Joscha Feth <joscha@feth.com>
2012-07-02 05:56:57 +0200

    remove unnecessary override
    Former-commit-id: a8b12f3d66a20a2ed861358183b7ba97a31d4686

--- code/app/com/feth/play/module/pa/providers/openid/OpenIdAuthProvider.java
+++ code/app/com/feth/play/module/pa/providers/openid/OpenIdAuthProvider.java
@@ -810,17 +810,6 @@ name|expires
 argument_list|)
 return|;
 block|}
-annotation|@
-name|Override
-specifier|public
-name|boolean
-name|isExternal
-parameter_list|()
-block|{
-return|return
-literal|true
-return|;
-block|}
 block|}
 end_class
 
